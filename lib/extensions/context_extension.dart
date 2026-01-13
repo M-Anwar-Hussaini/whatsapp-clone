36c3d7909f691e5a3fa617f46001c3690a100740
Anwar Hussaini <anwarhussaini160@gmail.com>
2026-01-13 12:37:54 +0430

    feat(extensions): add BuildContext extension for theme colors
    Introduces a new `ThemeExtension` on `BuildContext` to simplify access to
    commonly used theme colors.
    - Added `primary` getter to access `ColorScheme.primary`.
    - Added `onPrimary` getter to access `ColorScheme.onPrimary`.
    
    This reduces boilerplate by removing the need to repeatedly call
    `Theme.of(context).colorScheme`.

---
+++ lib/extensions/context_extension.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+extension ThemeExtension on BuildContext {
+  ThemeData get _theme => Theme.of(this);
+  Color get primary => _theme.colorScheme.primary;
+  Color get onPrimary => _theme.colorScheme.onPrimary;
+}
