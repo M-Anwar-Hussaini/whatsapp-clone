00f1bd0b0434fdcae203abc7a480c511082bec06
Anwar Hussaini <anwarhussaini160@gmail.com>
2026-01-18 16:35:25 +0430

    feat(widget): enhance CustomCard to display dynamic chat details and navigation
    This commit updates the `CustomCard` widget to accept a `ChatModel` instance, allowing it to display dynamic chat information such as the chat name and current message. It also implements navigation to the conversation screen when the card is tapped, improving user interaction within the chat interface.

--- lib/widgets/common/custom_card.dart
+++ lib/widgets/common/custom_card.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:whatsapp_clone/extensions/context_extension.dart';
+import 'package:whatsapp_clone/models/chat/chat_model.dart';
+import 'package:whatsapp_clone/router/route_names.dart';
 
 class CustomCard extends StatelessWidget {
-  const CustomCard({super.key});
+  const CustomCard({super.key, required this.chat});
+  final ChatModel chat;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap: () {
+        context.push(RouteNames.conversation, extra: chat);
+      },
       leading: CircleAvatar(
         backgroundColor: context.primary.withValues(alpha: 0.5),
-        child: Icon(Icons.groups, color: context.onPrimary),
+        child: Icon(chat.icon, color: context.onPrimary),
       ),
       title: Text(
-        'Dev Stack',
+        chat.name,
         style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
       ),
       subtitle: Row(
         children: [
           Icon(Icons.done_all, size: 16),
           const SizedBox(width: 4),
-          Text('Hi Dev Stack', style: TextStyle(fontSize: 13)),
+          Text(chat.currentMessage, style: TextStyle(fontSize: 13)),
         ],
       ),
       trailing: Text('18:20'),
-      onTap: () {},
     );
   }
 }
