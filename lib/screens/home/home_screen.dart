c3db2abf1c6b6ab6e0f8c96963ff5d68caba8a7b
Anwar Hussaini <anwarhussaini160@gmail.com>
2026-01-18 13:44:31 +0430

    feat(home): replace placeholder with ChatPage in tab view
    Replaces the temporary text widget with the actual ChatPage implementation in the HomeScreen's TabBarView to display the chat list.

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/screens/home/chat_page.dart';
 import 'package:whatsapp_clone/widgets/home/home_floating_action_button.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -28,16 +29,12 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    IndexedStack(children: []);
     return Scaffold(
       appBar: _buildAppBar(),
       body: TabBarView(
         controller: _controller,
-        children: [
-          Text('Camera'),
-          Text('Chats'),
-          Text('Updates'),
-          Text('Calls'),
-        ],
+        children: [Text('Camera'), ChatPage(), Text('Updates'), Text('Calls')],
       ),
       floatingActionButton: HomeFloatingActionButton(index: _controller.index),
     );
